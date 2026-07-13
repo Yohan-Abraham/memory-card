5b53c3349f8d0c2fe3aa52a2485e968aa2940263
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2026-07-13 15:05:23 -0700

    feat: implement pokemon and score tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,62 @@
 import './App.css';
 import Score from './components/score';
 import Cards from './components/cards';
+import { useEffect, useState } from 'react';
+import organizeData from './api/fetch';
+
+function randomizePokemons(pokemons) {
+  return [...pokemons].sort(() => Math.random() - 0.5);
+}
+
+function containsDuplicates(pokemons) {
+  let hashmap = new Map();
+
+  for (let i = 0; i < pokemons.length; i++) {
+    if (hashmap.has(pokemons[i])) {
+      return true;
+    }
+    hashmap.set(pokemons[i], pokemons[i]);
+  }
+  return false;
+}
 
 function App() {
+  const [currentPokemons, setCurrentPokemons] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [pokemons, setPokemon] = useState([]);
+  const [bestScore, setBestScore] = useState(0);
+
+  useEffect(() => {
+    async function loadPokemon() {
+      const data = await organizeData();
+      setPokemon(data);
+    }
+
+    loadPokemon();
+  }, []);
+
+  function handleShuffle(name) {
+    setPokemon(randomizePokemons(pokemons));
+    setCurrentPokemons([...currentPokemons, name]);
+    if (containsDuplicates(currentPokemons)) {
+      if (currentScore > bestScore) {
+        setBestScore(currentScore);
+      }
+      setCurrentScore(0);
+      setCurrentPokemons([]);
+    } else {
+      setCurrentScore(currentScore + 1);
+    }
+  }
+
   return (
     <>
       <header>
         <h1>PokéMemory</h1>
-        <Score></Score>
+        <Score score={currentScore} bestScore={bestScore}></Score>
       </header>
       <main>
-        <Cards></Cards>
+        <Cards pokemons={pokemons} handleShuffle={handleShuffle}></Cards>
       </main>
       <footer></footer>
     </>
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,32 +1,15 @@
 import '../styles/card.css';
-import { useEffect, useState } from 'react';
-import organizeData from '../api/fetch';
 
-function randomizePokemons(pokemons) {
-  return [...pokemons].sort(() => Math.random() - 0.5);
-}
-
-export default function Cards() {
-  const [pokemons, setPokemon] = useState([]);
-
-  useEffect(() => {
-    async function loadPokemon() {
-      const data = await organizeData();
-      setPokemon(data);
-    }
-
-    loadPokemon();
-  }, []);
-
-  function handleShuffle() {
-    setPokemon(randomizePokemons(pokemons));
-  }
-
+export default function Cards({ pokemons, handleShuffle }) {
   return (
     <section className="cards-container">
       {pokemons.map((pokemon) => {
         return (
-          <button key={pokemon.id} className="card" onClick={handleShuffle}>
+          <button
+            key={pokemon.id}
+            className="card"
+            onClick={() => handleShuffle(pokemon.name)}
+          >
             <div className="img-container">
               <img src={pokemon.image} alt={pokemon.name} />
             </div>
